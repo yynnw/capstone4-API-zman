eedc911138e58f02c0d6a7f40327362c6e130ddb
YW <yynnw86@gmail.com>
2024-08-26 04:51:23 -0400

    fix partials

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set('views', path.join(__dirname, 'views'));
+
 // Middleware to parse application/x-www-form-urlencoded data
 app.use(express.urlencoded({ extended: true }));
 
